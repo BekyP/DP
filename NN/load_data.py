ec0972afd53375eb364c7182d64a9b82bd23231e
Patrik Beka <patrik.beka@softec.sk>
2018-12-11 22:45:16 +0100

    Add missing images and pdf

--- NN/load_data.py
+++ NN/load_data.py
@@ -4,10 +4,10 @@ import numpy as np
 from utils import listdir_fullpath
 
 
-def load_data(folder, num_of_samples=None, read_flag=cv2.IMREAD_UNCHANGED):
-    return list(map(lambda x: cv2.imread(x, read_flag) / 255, sorted(listdir_fullpath(folder))[0:num_of_samples]))
+def load_data(folder, first=0, last=None, read_flag=cv2.IMREAD_UNCHANGED):
+    return list(map(lambda x: cv2.imread(x, read_flag) / 255, sorted(listdir_fullpath(folder))[first:last]))
 
 
-def load_data(folder, size, num_of_samples=None, read_flag=cv2.IMREAD_UNCHANGED):
+def load_data(folder, size, first=0, last=None, read_flag=cv2.IMREAD_UNCHANGED):
     return list(map(lambda x: cv2.resize(cv2.imread(x, read_flag), size) / 255,
-                    sorted(listdir_fullpath(folder))[0:num_of_samples]))
+                    sorted(listdir_fullpath(folder))[first:last]))
