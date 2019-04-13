74ec7912eaccf8b020e85beb23b6b47db830b08e
Patrik Beka <patrik.beka@gmail.com>
2019-04-13 13:44:59 +0200

    Add vgg16 + autoencoder model

---
+++ NN/vgg16_with_autoencoder/predict_vgg16_with_autoencoder.py
@@ -0,0 +1,90 @@
+import tensorflow as tf
+from keras import backend as K
+from keras.models import load_model
+
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+
+from scipy.misc import toimage
+
+import sys
+sys.path.append('../') 
+
+import nn_utils.metrics as metrics # source codes of metrics from: https://github.com/herrlich10/saliency
+from nn_utils.utils import listdir_fullpath
+from nn_utils.load_data import load_data
+from nn_utils.binary_map_with_fixations import get_binary_fixation_maps
+
+import matplotlib
+matplotlib.use('Agg')
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+K.set_session(session)
+
+
+parser = argparse.ArgumentParser(fromfile_prefix_chars='@', description='train neural network')
+parser.add_argument('--images', help='folder with input images', required=True)
+parser.add_argument('--maps', help='folder with heatmaps', required=True)
+parser.add_argument('--binary_maps', help='folder with binary fixations maps', required=True)
+parser.add_argument('--loss', help='loss function', required=True)
+parser.add_argument('--optimizer', help='optimizer', required=True)
+
+args = parser.parse_args()
+
+n = 224
+
+model = load_model("models/"+args.optimizer+"_"+args.loss+".model")
+
+model.compile(loss=args.loss,
+              optimizer=args.optimizer,
+              metrics=['accuracy'])
+
+model.summary()
+
+imgs = np.array(load_data(args.images, (n, n)))[5000:]
+img_names = sorted(listdir_fullpath(args.images))[5000:]
+
+predicted = model.predict(imgs, verbose=1)
+
+i = 0
+
+for p, img in zip(predicted, img_names):
+    i += 1
+    print(str(i) + ". saving " + str(img.rsplit('/', 1)[1]))
+    plt.imshow(p, cmap='jet')
+    toimage(p).save("predicted_maps/"+str(img.rsplit('/', 1)[1]))
+    plt.savefig("predicted_maps/plot_"+str(img.rsplit('/', 1)[1]))
+    if i == 50:
+        break
+
+def count_metrics(predicted_heatmaps, orig, binary_maps):  # computes metrics
+    cc = 0
+    auc = 0
+    similarity = 0
+    nss = 0
+    auc_s = 0
+    auc_b = 0
+
+    for map, original_map, fix in zip(predicted_heatmaps, orig, binary_maps):
+        auc += metrics.AUC_Judd(map, fix, True)
+        #auc_b += metrics.AUC_Borji(map, fix)
+        nss += metrics.NSS(map, fix)
+        auc_s += metrics.AUC_shuffled(map, fix, np.zeros([n,n]))
+        cc += metrics.CC(map, original_map)
+        similarity += metrics.SIM(map, original_map)
+
+    num=len(orig)
+    print("final correlation coeficient: " + str(cc / num))
+    print("final SIM: " + str(similarity / num))
+    print("final NSS: " + str(nss / num))
+    print("final judd AUC: " + str(auc / num))
+    print("final shuffled AUC: " + str(auc_s / num))
+    print("final borji AUC: " + str(auc_s / num))
+
+original = np.array(load_data(args.maps, (n, n)))[5000:]
+binary_maps = np.array(get_binary_fixation_maps(args.binary_maps, size=n, first=5000))
+
+count_metrics(predicted, original, binary_maps)
---
+++ NN/vgg16_with_autoencoder/train_vgg16_with_autoencoder.py
@@ -0,0 +1,123 @@
+import tensorflow as tf
+import sys
+sys.path.append('../') 
+from keras.applications.vgg16 import VGG16
+from keras.layers import *
+from keras.models import Model, Sequential
+from keras.utils import plot_model
+from keras.callbacks import ModelCheckpoint, TensorBoard, EarlyStopping
+
+from keras import backend as K
+import argparse
+
+from nn_utils.load_data import load_data 
+from nn_utils.utils import get_model_memory_usage
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+K.set_session(session)
+
+parser = argparse.ArgumentParser(fromfile_prefix_chars='@', description='train neural network')
+parser.add_argument('--images', help='folder with input images', required=True)
+parser.add_argument('--maps', help='folder with saliency maps', required=True)
+parser.add_argument('--loss', help='loss function', required=True)
+parser.add_argument('--optimizer', help='optimizer', required=True)
+parser.add_argument('--conv_layers', help='number of conv layers', type=int, default=3)
+parser.add_argument('--batch_size', help='batch size', type=int, default=10)
+parser.add_argument('--epochs', help='number of epochs', type=int, default=500)
+
+args = parser.parse_args()
+conv_layers = args.conv_layers
+
+input_layer=Input(shape=(224, 224, 3))
+
+model = VGG16(input_tensor=input_layer)
+
+#model.summary()
+
+n = 224
+
+x = Conv2D(filters=3, kernel_size=3, activation='relu', padding='same')(input_layer)
+x = MaxPooling2D((2, 2), padding='same')(x)  
+
+resized_n = n 
+
+for i in range(0, conv_layers-1):
+    x = Conv2D(filters=3, kernel_size=3, activation='relu', padding='same')(x)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    resized_n /= 2
+
+x = Flatten()(x)
+resized_n = int(resized_n/2)
+encoded = Dense(resized_n*resized_n)(x)
+x = Reshape((resized_n,resized_n,1))(encoded)
+x = Conv2D(filters=3, kernel_size=3, activation='relu', padding='same')(x)
+
+autoencoder = Model(input_layer, x)
+
+autoencoder.summary()
+
+small_model= Sequential()
+
+vgg16_layers_num = 6+(conv_layers-1)*4
+
+for layer in model.layers[:vgg16_layers_num]:
+    small_model.add(layer)
+
+for i in range(0, len(model.layers)-vgg16_layers_num):
+	model.layers.pop()
+
+for layer in small_model.layers:
+	layer.trainable = False
+
+model=None
+small_model.summary()
+
+x = Concatenate()([small_model.output, autoencoder.output])
+
+print(K.int_shape(x))
+
+for i in range(0, conv_layers):
+    x = Conv2D(filters=3, kernel_size=3, activation='relu', padding='same')(x)
+    x = UpSampling2D((2, 2))(x) 
+
+#x = Conv2D(filters=3, kernel_size=3, activation='relu', padding='same')(x)
+#x = UpSampling2D((2, 2))(x)
+
+x = Conv2D(filters=1, kernel_size=5, activation='sigmoid', padding='same')(x)
+print(K.int_shape(x))
+
+x = Reshape((n, n))(x)
+
+final = Model(input_layer, x) 
+small_model = None
+autoencoder = None
+
+final.compile(optimizer=args.optimizer, loss=args.loss)
+final.summary()
+
+
+#plot_model(final, to_file='model.png', show_shapes=True)
+
+#print("saved in model.png")
+
+images = np.array(load_data(args.images, (n, n), last=5000))
+maps = np.array(load_data(args.maps, (n, n), last=5000))
+
+split = int(0.9 * len(images))
+
+train_images = images[:split]
+train_maps = maps[:split]
+
+valid_images = images[split:]
+valid_maps = maps[split:]
+
+print("memory usage: " + str(get_model_memory_usage(args.batch_size, final)) + " GB")
+
+model_name = args.optimizer + "_" + args.loss + ".model"
+final.fit(train_images, train_maps, epochs=args.epochs, batch_size=args.batch_size,
+				shuffle=True, validation_data=(valid_images, valid_maps), 
+				verbose=5, callbacks=[ModelCheckpoint("models/"+model_name,monitor='val_loss', verbose=3, save_best_only=True), 
+									  TensorBoard(log_dir='logs', histogram_freq=0, write_graph=True, write_images=True), 
+									  EarlyStopping(monitor='val_loss', patience=10)])
